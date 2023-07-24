ff3501726a9dd5c91235c8e719b351988e84726a
shadyk89 <46767745+shadyk89@users.noreply.github.com>
2023-07-24 11:18:14 -0400

    Create BinaryTree.java
    Outline for BinaryTree class

---
+++ BinaryTree.java
@@ -0,0 +1,26 @@
+//import libs
+import java.util.*;
+
+/*BinaryTree class
+  Implementation of BinaryTree
+*/
+public class BinaryTree{
+  //fields
+
+  //constructors
+
+  //methods
+  //insert  -Henry
+  //remove
+  //search  -Henry
+  //traverse
+
+  //Node class  -Henry
+  
+  //driver func/test script
+  public static void main(String[] args){
+    //vars
+
+    //test methods
+  }
+}
