e18d7560b6c44e601b1f85df4a9da5e5ee7526b3
shadyk89 <46767745+shadyk89@users.noreply.github.com>
2023-07-24 14:23:25 -0400

    Updated BinaryTree.java with Henry Kim contributions
    Added inner class Node.
    Added insert method.
    Added search method.

--- BinaryTree.java
+++ BinaryTree.java
@@ -6,21 +6,115 @@ import java.util.*;
 */
 public class BinaryTree{
   //fields
+  Node root;
 
   //constructors
-
+	BinaryTree(){
+		root = null;
+	}
+	
   //methods
-  //insert  -Henry
+  /*insert method that recursively adds data to the binary tree
+		written by Henry Kim
+  */
+	public void insert(int data){
+		root = insertRecursive(root, data);
+	}
+	
+	/*recursive portion of insert method
+		starting from root, check if current node is null
+		if null add new node with data
+		otherwise check if data goes to left or right of current node
+		written by Henry Kim
+  */
+	private Node insertRecursive(Node current, int data){
+		if (current == null){
+			return new Node(data);
+		}
+		
+		if (data < current.data){
+			current.left = insertRecursive(current.left, data);
+		}
+		else if(data > current.data){
+			current.right = insertRecursive(current.right, data);
+		}
+		else{
+			return current;
+		}
+		
+		return current;
+	}
+	
   //remove
-  //search  -Henry
+	
+	
+  /*search method that recursively searches for data in binary tree
+		written by Henry Kim
+  */
+	public boolean search(int data){
+		return searchRecursive(root, data);
+	}
+	
+	/*recursive portion of search method
+		starting from root, check if current node is null
+		if null return false
+		otherwise check if data may exist left or right of current node
+		if data exists return true
+		written by Henry Kim
+  */
+	private boolean searchRecursive(Node current, int data){
+		if (current == null){
+			return false;
+		}
+		
+		if (data == current.data){
+			return true;
+		}
+		
+		if (data < current.data){
+			return searchRecursive(current.left, data);
+		}
+		else{
+			return searchRecursive(current.right, data);
+		}
+	}
   //traverse
 
-  //Node class  -Henry
+  /*Node class containing int for data, as well as pointers to left and right Nodes
+		written by Henry Kim
+  */
+	public class Node{
+		int data;
+		Node left, right;
+		
+		public Node(){
+			this(0);
+		}
+		
+		public Node(int data){
+			this.data = data;
+			left = right = null;
+		}
+	}
   
   //driver func/test script
   public static void main(String[] args){
     //vars
-
-    //test methods
+		BinaryTree bt = new BinaryTree();
+		
+		//test methods
+		
+		//insert test
+		bt.insert(5);
+		bt.insert(6);
+		bt.insert(4);
+		
+		System.out.println(bt.root.data);
+		System.out.println(bt.root.left.data);
+		System.out.println(bt.root.right.data);
+		
+		//search test
+		System.out.println(bt.search(3));
+		System.out.println(bt.search(4));
   }
 }
